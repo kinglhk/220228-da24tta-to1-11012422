cdee18ec27eb1363e45c3ce1ee25a457027dc001
kinglhk <kingkey.lhk@gmail.com>
2024-11-12 14:59:00 +0700

    Add files via upload

--- vd2.cpp
+++ vd2.cpp
@@ -9,10 +9,11 @@ int main()
 	printf("\n nhap ma so sinh vien:"); fflush(stdin); scanf("%s",mssv);
 	printf("\n nhap ho va ten:"); fflush(stdin); gets(fullname);
 	printf("\n nhap lop:"); fflush(stdin); scanf("%s",lop);
-	printf("\n nhap nam sinh:"); scanf("%d", ns);
-	printf("\n nhap diem xet tuyen:"); scanf("%f",diem);
-	
+	printf("\n nhap nam sinh:"); scanf("%d",&ns);
+	printf("\n nhap diem xet tuyen:"); scanf("%f",&diem);
+
 	//xuat
+	printf("\n");
 	printf("\nthong tin sinh vien");
 	printf("\n ma so sinh vien: %s",mssv);
 	printf("\n ho va ten: %s",fullname);
