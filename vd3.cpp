8a3f896b5f0ad29f6fcdbe2b8a1459936a0229e5
kinglhk <kingkey.lhk@gmail.com>
2024-11-12 15:31:37 +0700

    Add files via upload

---
+++ vd3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int i, j;
+	printf("\n nhap gia tri cho i:"); scanf("%d",&i);
+	printf("\n nhap gia tri cho j:"); scanf("%d",&j);
+	if (i<j)
+		printf("\n so be hon la :%d",i);
+		else
+		printf("\n so be hon la :%d",j);
+	return 0;
+}
