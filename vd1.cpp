387e211fc8a2019a3c556fe8d27e7f7e73bbd626
kinglhk <kingkey.lhk@gmail.com>
2024-11-12 13:54:36 +0700

    Add files via upload

---
+++ vd1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	printf ("\n mssv 11012422");
+	printf ("\n hoten luong hoang khoa");
+	printf ("\n lop da24tta");
+	printf ("\n namsinh 22072006");
+	printf ("\n dxt 18");
+	return 0;
+}
