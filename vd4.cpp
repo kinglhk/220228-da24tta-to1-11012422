c7c3ccb1cc25b2a21ce13c7065b1656700163efe
kinglhk <kingkey.lhk@gmail.com>
2024-11-12 15:49:40 +0700

    Add files via upload

---
+++ vd4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int Time;
+	printf ("\n nhap so gio hien tai:"); scanf("%d",&Time);
+	if (Time<18)
+		printf ("\n Have a good day!");
+		else
+		printf ("\n Have a good night!");
+	return 0;
+}
